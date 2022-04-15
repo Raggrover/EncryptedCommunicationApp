b5b223214323492436b7b5c704a15bdee4ddd199
unknown <raghavgrover5@gmail.com>
2022-04-15 13:14:53 +0100

    Added socket functions in server

--- src/server/server.js
+++ src/server/server.js
@@ -91,3 +91,131 @@ function createChannel(name, user, p2p) {
     chat.sockets.connected[user.socketid].join(name); // add admin to self chat
     return channel;
 }
+
+function defineSocketEvents(socket) {
+
+    socket.on('disconnect', () => {
+        // Sockets will leave all the chats they were part of automatically 
+
+        var user = socket.user || manager.findUser(socket.id);
+        if (user) {
+            console.warn(`User <${user.username}> by socket <${user.socketid}> disconnected!`);
+            user.status = "offline";
+
+            socket.broadcast.to(globalChannel).emit('leave',
+                { username: user.username, id: user.id, avatar: user.avatar, status: user.status });
+        }
+    });
+
+    //managing when user is sending msg
+    socket.on("msg", data => {
+        var from = socket.user || manager.findUser(socket.id);
+        var channel = manager.channels[data.to];
+
+        if (from != null && channel != null && channel.users.indexOf(from.id) != -1) {
+            var msg = manager.messages[channel.name];
+            if (msg == null)
+                msg = manager.messages[channel.name] = [];
+
+            data.date = Date.now();
+            data.type = "msg";
+            chat.sockets.in(channel.name).emit('receive', data);
+            msg.push(data);
+        }
+    });
+
+    socket.on("request", data => {
+
+        // find user who requested to this chat by socket id
+        var from = socket.user || manager.findUser(socket.id);
+
+        // if user is available 
+        if (from) {
+            data.from = from.id;
+
+            var adminUser = manager.getAdminFromChannelName(data.channel, from.id)
+
+            if (adminUser) {
+                if (adminUser.status == "offline") {
+                    var p2p = (manager.channels[data.channel] == null ? true : manager.channels[data.channel].p2p);
+                    socket.emit("reject", { from: adminUser.id, channel: data.channel, p2p: p2p, msg: "admin user is offline" });
+                }
+                else
+                    chat.to(adminUser.socketid).emit("request", data)
+                return;
+            }
+        }
+        //if admin is not found, handled null pointer exception
+        socket.emit("exception", "The requested chat not found!");
+    });
+
+        
+    socket.on("accept", data => {
+
+        var from = socket.user || manager.findUser(socket.id);
+
+        var to = manager.clients[data.to];
+
+        if (from != null && to != null) {
+            var channel = manager.channels[data.channel];
+
+            if (channel == null) {
+                channel = createChannel(data.channel, from, true)
+            }
+            
+            channel.users.push(to.id);
+            chat.sockets.connected[to.socketid].join(channel.name); // add new user to chat channel
+
+            socket.to(to.socketid).emit("accept", { from: from.id, channel: channel.name, p2p: channel.p2p, channelKey: data.channelKey })
+        }
+    });
+
+    socket.on("createChannel", name => {
+        var from = socket.user;
+        var channel = manager.channels[name];
+
+        if (channel) {
+            socket.emit("reject", { from: from.id, p2p: false, channel: channel, msg: "The given channel name is already exist" })
+            return;
+        }
+
+        channel = createChannel(name, from, false);
+        updateAllUsers();
+
+        console.info(`Channel <${channel.name}> created by user <${from.username}: ${channel.adminUserId}>`)
+    });
+
+
+    socket.on("reject", data => {
+
+        var from = socket.user || manager.findUser(socket.id);
+
+        var to = manager.clients[data.to];
+
+        if (from != null && to != null) {
+            var channel = manager.channels[data.channel];
+            socket.to(to.socketid).emit("reject", { from: from.id, p2p: (channel == null), channel: data.channel })
+        }
+    });
+
+    socket.on("fetch-messages", channelName => {
+        var fetcher = socket.user || manager.findUser(socket.id);
+
+        var channel = manager.channels[channelName];
+
+        if (fetcher != null && channel != null && channel.users.indexOf(fetcher.id) !== -1)
+            socket.emit("fetch-messages", { channel: channel.name, messages: manager.messages[channel.name] });
+        else
+            socket.emit("exception", `you are not joined in <${channelName}> channel or maybe the server was lost your data!!!`);
+    });
+
+    socket.on("typing", channelName => {
+        var user = socket.user || manager.findUser(socket.id);
+        var channel = manager.channels[channelName];
+
+        if (user && channel && channel.users.indexOf(user.id) !== -1) {
+            chat.sockets.in(channel.name).emit("typing", { channel: channel.name, user: user.id });
+        }
+    });
+    
+}
